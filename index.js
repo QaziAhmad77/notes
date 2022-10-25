6811eb12dcd20ab983202291cc39132fb10b7129
Qazi Ahmad <107860183+QaziAhmad77@users.noreply.github.com>
2022-10-25 10:22:28 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,60 +1,99 @@
 console.log("This is working");
+showNotes();
 
-// Initialize the news api parameters
-var apiKey = "e92e34bf4fbc4b699ee77c739f997ab5";
-var source = "al-jazeera-english";
-// ary-news
-// cnn
-// bbc-news
-// bbc-sports
-// Metro
-// new-york-magazine
-// etc
+// // If user adds a note, add it to the localStorage
 
-// Grab the news container
-let newsAccordion = document.getElementById("newsAccordion");
-
-
-// Create an Ajex get request
-const xhr = new XMLHttpRequest();
-// xhr.open("GET","https://newsapi.org/v2/top-headlines?sources=BBc-news&apiKey=e92e34bf4fbc4b699ee77c739f997ab5", true);
-xhr.open("GET", `https://newsapi.org/v2/top-headlines?sources=${source}&apiKey=${apiKey}`, true);
-
-xhr.onload = function () {
-    if (this.status === 200) {
-        let json = JSON.parse(xhr.responseText);
-        let articles = json.articles;
-        // console.log(articles);
-
-        let newsHTML = " ";
-        articles.forEach(function(element,index){
-            // console.log(articles[news]);
-            newsHTML += `
-        <div class="card">
-            <div class="card-header" id="heading${index}">
-                <h5 class="mb-0">
-                    <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapse${index}"
-                        aria-expanded="true" aria-controls="collapse${index}">
-                        <b>Breaking News${index+1}:</b>${element["title"]};
-                    </button>
-                </h5>
-            </div>
-
-            <div id="collapse${index}" class="collapse" aria-labelledby="heading${index}"
-                data-parent="#newsAccordion">
-                <div class="card-body">${element["content"]}. <a href="${element["url"]}" target="_blank">Read more here</a></div>
-        </div>
-        </div>`
-        });
-        newsAccordion.innerHTML = newsHTML;
+let addBtn = document.getElementById('addBtn');
+addBtn.addEventListener('click', function (e) {
+    let addTxt = document.getElementById('addTxt');
+    let addTitle = document.getElementById('addTitle');
+    let notes = localStorage.getItem('notes');
+    if (notes == null) {
+        notesObj = [];
     }
     else {
-        console.log("Error");
+        notesObj = JSON.parse(notes);  // it will convert string into an array
+    }
+    let myObj = {
+        title: addTitle.value,
+        text: addTxt.value
+    }
+    notesObj.push(myObj);
+    localStorage.setItem('notes', JSON.stringify(notesObj)); // it will convert an array or object into string
+    addTxt.value = "";
+    addTitle.value = "";
+    showNotes();
+});
+
+
+// // Function to show elements from localStorage.
+function showNotes() {
+    let notes = localStorage.getItem('notes');
+    if (notes == null) {
+        notesObj = [];
+    }
+    else {
+        notesObj = JSON.parse(notes);  // it will convert string into an array
+    }
+    let html = "";
+    notesObj.forEach(function (element, index) {
+        html += `
+        <div class="noteCard my-2 mx-2 card" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">${element.title}</h5>
+                    <p class="card-text">${element.text}</p>
+                    <button id="${index}" onclick= "deleteNote(this.id)" class="btn btn-primary">Delete Note</button>
+                </div>
+            </div>`
+
+    });
+    let notesElm = document.getElementById('notes');
+    if (notesObj.length != 0) {
+        notesElm.innerHTML = html;
+    }
+    else {
+        notesElm.innerHTML = `Nothing to show! Use "Add a Note" section above to add notes.`;
     }
 };
 
-xhr.send();
+
+// Function to delete a note.
+
+function deleteNote(index) {
+    let notes = localStorage.getItem('notes');
+    if (notes == null) {
+        notesObj = [];
+    }
+    else {
+        notesObj = JSON.parse(notes);  // it will convert string into an array
+    }
+    notesObj.splice(index, 1); // remove
+    localStorage.setItem('notes', JSON.stringify(notesObj));
+    showNotes();
+}
 
 
 
+let search = document.getElementById('searchTxt');
+search.addEventListener('input', function () {
+    let inputVal = search.value.toLowerCase();
+    // console.log("Input event fired", inputVal);
+    let noteCard = document.getElementsByClassName("noteCard");
+
+    Array.from(noteCard).forEach(function (element) {
+        let carTxt = element.getElementsByTagName("p")[0].innerText;
+        // console.log(carTxt);
+        if (carTxt.includes(inputVal)) {
+            element.style.display = "block";
+        }
+        else {
+            element.style.display = "none";
+        }
+    });
+})
+
+
+
+let date = new Date();
+document.write(date);
 
